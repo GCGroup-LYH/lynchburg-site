f3c4e9d0cf9b56b7b28a16dcbe82e66d752d5de9
Chris Collier <emcITconsultants@outlook.com>
2026-03-29 18:42:45 -0400

    Confirmed working build

--- .eleventy.js
+++ .eleventy.js
@@ -1,25 +1,13 @@
-const eleventySass = require("eleventy-sass");
 
 module.exports = function(eleventyConfig) {
   
-  //  ADD THE SASS PLUGIN (The new "Engine")
-  eleventyConfig.addPlugin(eleventySass, {
-    compileOptions: {
-      permalink: function(contents, inputPath) {
-        // This ensures your SCSS file becomes a CSS file in the build folder
-        return (data) => "/assets/css/main.css";
-      }
-    }
-  });
 
-  //  KEEP EXISTING PASSTHROUGHS (The "Body")
+
   eleventyConfig.addPassthroughCopy("src/admin");
   eleventyConfig.addPassthroughCopy("src/assets/images");
   eleventyConfig.addPassthroughCopy("src/assets/fonts");
   eleventyConfig.addPassthroughCopy("src/_redirects");
-
-  // Note: We removed src/assets/css passthrough because 
-  // the Sass plugin is now "creating" that folder automatically.
+  eleventyConfig.addPassthroughCopy("src/assets");
 
   return {
     dir: {
